90493f9880fe61ebb014c81bda8f05737f0006a2
Bruna Di Bisceglie <bruh.mdb@gmail.com>
2015-03-29 18:27:12 -0300

    reconhecimento de acentos e maiúsculas  e correção do boneco

--- Untitled.py
+++ Untitled.py
@@ -20,7 +20,7 @@ janela.bgcolor ("purple")
 janela.title("jogo da forca")
 lapis = turtle.Turtle()
 lapis.speed(20)
-lapis.color("black")
+lapis.color("green")
 lapis.penup()
 lapis.setpos(-250,250)
 lapis.pendown()
@@ -158,70 +158,303 @@ def DesenharForca ():
 
 #BONECO
 def Cabeça ():
-    lapis.right(90)
+    lapis.penup()
+    lapis.setpos(-100, 70)
+    lapis.pendown()
     lapis.circle(40)
-    lapis.left(90)
     
 def Corpo ():
+    lapis.left(90)
     lapis.penup()
-    lapis.forward(80)
+    lapis.setpos(-100, 70)
     lapis.pendown()
-    lapis.forward(100)
-    lapis.backward(70)
+    lapis.backward(100)
+    lapis.right(90)
 
 def Braço1 ():
-    lapis.left(45)
-    lapis.forward(50)
-
-def Braço2 ():
-    lapis.backward(50)
-    lapis.right(90)
-    lapis.forward(50)
-
-def Perna1 ():
-    lapis.backward(50)
-    lapis.left(45)
-    lapis.forward(70)
-    lapis.left(45)
-    lapis.forward(50)
-
-def Perna2 ():
-    lapis.backward(50)
-    lapis.right(90)
+    lapis.penup()
+    lapis.setpos(-100, 40)
+    lapis.pendown()
+    lapis.left(135)
     lapis.forward(50)
     lapis.right(135)
+
+def Braço2 ():
+    lapis.penup()
+    lapis.setpos(-100, 40)
+    lapis.pendown()
+    lapis.left(45)
+    lapis.forward(50)
+    lapis.right(45)
+
+def Perna1 ():
+    lapis.penup()
+    lapis.setpos(-100, -30)
+    lapis.pendown()
+    lapis.left(135)
+    lapis.forward(50)
+    lapis.right(135)
+
+def Perna2 ():
+    lapis.penup()
+    lapis.setpos(-100, -30)
+    lapis.pendown()
+    lapis.right(135)
+    lapis.forward(50)
+    lapis.left(135)
     
 def ColocandoNoLugarParaTracinho ():
     lapis.penup()
     lapis.setpos(-200, -250)
+    lapis.left(90)
+    lapis.pendown()
 
 def TracinhoDaLetra():
-    lapis.forward(20)
+    lapis.forward(10)
     lapis.penup()
     lapis.forward(10)
     lapis.pendown()
-
-jogar = "sim"
-while jogar == "sim":
+#fazer tracinhos de acordo com tamanho da palavra
+def Tracinho(palavra):
+    for c in palavra:
+        if c == " ":
+            lapis.penup()
+            lapis.forward(20)
+            lapis.pendown()
+        else:
+            TracinhoDaLetra()
+boneco = 0
+while lista != [] and boneco < 6:
     #escolher palavra da lista
+    lapis.clear()
     DesenharForca()
+    ColocandoNoLugarParaTracinho()
+    
     from random import choice
     palavra = choice (lista)
+    print (palavra)
     lista = lista.remove(palavra)
-    #fazer tracinhos de acordo com tamanho da palavra
-    def Tracinho(palavra):
-        for c in palavra:
-            if c == " ":
-                lapis.penup()
-                lapis.forward(30)
-                lapis.pendown()
-            else:
-                TracinhoDaLetra()
-    Tracinho (palavra)
-    import window
-    letra = window.textinput("Inserir Letra", "Insira uma letra")
-    if letra in palavra:
-        lugardaletra = palavra.find(letra)
+    Tracinho(palavra)
+    letras = []
+    boneco = 0
+    qt = 0
+    if " "in palavra:
+        tamanho = len(palavra) -1
+    else:
+        tamanho = len(palavra)
+    while boneco <= 6 and qt < tamanho:
+        lapis.penup()
+        lapis.setpos(-200, -250)
+        lapis.pendown()
+        letra = janela.textinput("Inserir Letra", "Insira uma letra")
+        while letra in letras:
+            letra = janela.textinput("Inserir Letra", "Essa letra já foi, escolha outra:")
+        letras.append(letra)
+        if letra in palavra or (letra == "a" and palavra.find("ã") != -1) or (letra == "e" and palavra.find("E") != -1) or (letra == "i"and palavra.find("í") != -1) or (letra == "o" and palavra.find("ô") != -1) or (letra == "o" and palavra.find("ó") != -1) or (letra == "b" and palavra.find("B") != -1) or (letra == "s" and palavra.find("S") != -1) or (letra == "p" and palavra.find("P") != -1):
+            if letra in palavra:
+                caso1 = 0
+                ir = 1
+                while ir > 0:
+                    nletras = len(palavra) 
+                    voltar = nletras*20
+                    ir = (palavra.find(letra, caso1))*20
+                    if ir > 0:
+                        caso1 = palavra.find(letra, caso1) +1
+                        lapis.penup()
+                        lapis.forward(ir)
+                        lapis.write(letra, font = ("Arial", 20))
+                        lapis.penup()
+                        lapis.backward(ir)
+                        qt = qt +1
+            if letra == "a":
+                if palavra.find("ã") != -1:
+                    caso1 = 0
+                    ir = 1
+                    while ir > 0:
+                        nletras = len(palavra) 
+                        voltar = nletras*20
+                        ir = (palavra.find("ã", caso1))*20
+                        if ir > 0:
+                            caso1 = palavra.find("ã", caso1) +1
+                            lapis.penup()
+                            lapis.forward(ir)
+                            lapis.write("ã", font = ("Arial", 20))
+                            lapis.penup()
+                            lapis.backward(ir)
+                            qt = qt +1
+            if letra == "a":
+                if palavra.find("A") != -1:
+                    caso1 = 0
+                    ir = 1
+                    while ir > 0:
+                        nletras = len(palavra) 
+                        voltar = nletras*20
+                        ir = (palavra.find("A", caso1))*20
+                        if ir > 0:
+                            caso1 = palavra.find("A", caso1) +1
+                            lapis.penup()
+                            lapis.forward(ir)
+                            lapis.write("A", font = ("Arial", 20))
+                            lapis.penup()
+                            lapis.backward(ir)
+                            qt = qt +1
+            if letra == "e":
+                if palavra.find("E") != -1:
+                    caso1 = 0
+                    ir = 1
+                    while ir > 0:
+                        nletras = len(palavra) 
+                        voltar = nletras*20
+                        ir = (palavra.find("E", caso1))*20
+                        if ir > 0:
+                            caso1 = palavra.find("E", caso1) +1
+                            lapis.penup()
+                            lapis.forward(ir)
+                            lapis.write("E", font = ("Arial", 20))
+                            lapis.penup()
+                            lapis.backward(ir)
+                            qt = qt +1
+            if letra == "i":
+                if palavra.find("í") != -1:
+                    caso1 = 0
+                    ir = 1
+                    while ir > 0:
+                        nletras = len(palavra) 
+                        voltar = nletras*20
+                        ir = (palavra.find("í", caso1))*20
+                        if ir > 0:
+                            caso1 = palavra.find("í", caso1) +1
+                            lapis.penup()
+                            lapis.forward(ir)
+                            lapis.write("í", font = ("Arial", 20))
+                            lapis.penup()
+                            lapis.backward(ir)
+                            qt = qt +1
+            if letra == "o":
+                if palavra.find("ô") != -1:
+                    caso1 = 0
+                    ir = 1
+                    while ir > 0:
+                        nletras = len(palavra) 
+                        voltar = nletras*20
+                        ir = (palavra.find("ô", caso1))*20
+                        if ir > 0:
+                            caso1 = palavra.find("ô", caso1) +1
+                            lapis.penup()
+                            lapis.forward(ir)
+                            lapis.write("ô", font = ("Arial", 20))
+                            lapis.penup()
+                            lapis.backward(ir)
+                            qt = qt +1
+                if palavra.find("ó") != -1:
+                    caso1 = 0
+                    ir = 1
+                    while ir > 0:
+                        nletras = len(palavra) 
+                        voltar = nletras*20
+                        ir = (palavra.find("ó", caso1))*20
+                        if ir > 0:
+                            caso1 = palavra.find("ó", caso1) +1
+                            lapis.penup()
+                            lapis.forward(ir)
+                            lapis.write("ó", font = ("Arial", 20))
+                            lapis.penup()
+                            lapis.backward(ir)
+                            qt = qt +1
+            if letra == "b":
+                if palavra.find("B") != -1:
+                    caso1 = 0
+                    ir = 1
+                    while ir > 0:
+                        nletras = len(palavra) 
+                        voltar = nletras*20
+                        ir = (palavra.find("B", caso1))*20
+                        if ir > 0:
+                            caso1 = palavra.find("B", caso1) +1
+                            lapis.penup()
+                            lapis.forward(ir)
+                            lapis.write("B", font = ("Arial", 20))
+                            lapis.penup()
+                            lapis.backward(ir)
+                            qt = qt +1
+            if letra == "c":
+                if palavra.find("C") != -1:
+                    caso1 = 0
+                    ir = 1
+                    while ir > 0:
+                        nletras = len(palavra) 
+                        voltar = nletras*20
+                        ir = (palavra.find("C", caso1))*20
+                        if ir > 0:
+                            caso1 = palavra.find("C", caso1) +1
+                            lapis.penup()
+                            lapis.forward(ir)
+                            lapis.write("C", font = ("Arial", 20))
+                            lapis.penup()
+                            lapis.backward(ir)
+                            qt = qt +1
+            if letra == "s":
+                if palavra.find("S") != -1:
+                    caso1 = 0
+                    ir = 1
+                    while ir > 0:
+                        nletras = len(palavra) 
+                        voltar = nletras*20
+                        ir = (palavra.find("S", caso1))*20
+                        if ir > 0:
+                            caso1 = palavra.find("S", caso1) +1
+                            lapis.penup()
+                            lapis.forward(ir)
+                            lapis.write("S", font = ("Arial", 20))
+                            lapis.penup()
+                            lapis.backward(ir)
+                            qt = qt +1
+            if letra == "p":
+                if palavra.find("P") != -1:
+                    caso1 = 0
+                    ir = 1
+                    while ir > 0:
+                        nletras = len(palavra) 
+                        voltar = nletras*20
+                        ir = (palavra.find("P", caso1))*20
+                        if ir > 0:
+                            caso1 = palavra.find("P", caso1) +1
+                            lapis.penup()
+                            lapis.forward(ir)
+                            lapis.write("P", font = ("Arial", 20))
+                            lapis.penup()
+                            lapis.backward(ir)
+                            qt = qt +1
+            
+        elif boneco == 0:
+            Cabeça()
+            boneco = 1
+        elif boneco ==1:
+            Corpo()
+            boneco =2
+        elif boneco==2:
+            Braço1()
+            boneco = 3
+        elif boneco == 3:
+            Braço2()
+            boneco = 4
+        elif boneco == 4:
+            Perna1 ()
+            boneco = 5
+        elif boneco == 5:
+            Perna2 ()
+            boneco = 6
+            lapis.clear()
+            lapis.penup()
+            lapis.setpos(-250, 0)
+            lapis.write("Fim do jogo", font = ("Arial", 100))
+
+lapis.write("Você ganhou!", font = ("Arial", 100))
+#            
+            
         
+
+
+
 
 janela.exitonclick()
